86713400f7eb9034cf175aa54fce5bc9ed6e3303
kristasoos <99314662+kristasoos@users.noreply.github.com>
2022-04-16 13:59:31 +0800

    Removed console.log

--- src/index.js
+++ src/index.js
@@ -67,7 +67,6 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "7c66fd2eccd6dd57aa4d32051c6fbb9f";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
@@ -76,7 +75,6 @@ function getForecast(coordinates) {
 //Week 5 homework: Show temperature by city
 
 function currentWeatherData(response) {
-  console.log(response.data);
   let currentTemp = Math.round(response.data.main.temp);
   let tempCity = document.querySelector("#temperature");
   tempCity.innerHTML = `${currentTemp}`;
